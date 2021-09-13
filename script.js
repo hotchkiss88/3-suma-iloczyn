cca203e6ef9d44447ad751f09b7d08b7159cdba7
Grzegorz Sowinski <88460670+hotchkiss88@users.noreply.github.com>
2021-09-13 11:15:04 +0000

    adding a second option

--- script.js
+++ script.js
@@ -14,3 +14,14 @@ function mF(x) {
 };
 
 mF([1, 2, 3, 4, 5, 6]);
+
+/* function mF(table = [1, 2, 3, 4, 5, 6]) {
+    let resultConjunction = 1;
+    let resultAdd = 0;
+    table.forEach(function(x, i) {
+        resultConjunction = resultConjunction * x;
+        resultAdd = resultAdd + x;
+    });
+    console.log(resultAdd, resultConjunction);
+}
+mF(); */
